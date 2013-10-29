96916c374a5861dd29e259f0b8078a20180c7bc5
Jani Hirvinen <jani.hirvinen@gmail.com>
2013-10-29 09:38:14 +0700

    adding BSD license on all files

--- jD_IOBoard_RGB/jD_IOBoard_RGB.ino
+++ jD_IOBoard_RGB/jD_IOBoard_RGB.ino
@@ -1,24 +1,37 @@
-/*
- Copyright (c) 2012.  All rights reserved.
- An Open Source Arduino based jD_IOBoard 
- 
- Program  : jD_IOBoard_RGB Demo
- Version  : V1.0, June 06 2012
- Author(s): Jani Hirvinen
- 
- This program is free software: you can redistribute it and/or modify
- it under the terms of the GNU General Public License as published by
- the Free Software Foundation, either version 3 of the License, or
- (at your option) any later version.
- 
- This program is distributed in the hope that it will be useful,
- but WITHOUT ANY WARRANTY; without even the implied warranty of
- MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
- GNU General Public License for more details.
- 
- You should have received a copy of the GNU General Public License
- along with this program. If not, see <http://www.gnu.org/licenses/>
- 
+///////////////////////////////////////////////////////////////////////////////////////
+//
+// Name         : jD-IOBoard_RGB Demo
+// Version      : v1.0 06-06-2012
+// Author       : Jani Hirvinen, jani@j....com
+// Co-Author(s) :
+// 
+//
+// Copyright (c) 2013, Jani Hirvinen, jDrones & Co.
+// All rights reserved.
+//
+// - Redistribution and use in source and binary forms, with or without 
+//   modification, are permitted provided that the following conditions are met:
+//
+// - Redistributions of source code must retain the above copyright notice, this 
+//  list of conditions and the following disclaimer.
+//
+// - Redistributions in binary form must reproduce the above copyright notice, 
+//  this list of conditions and the following disclaimer in the documentation 
+//  and/or other materials provided with the distribution.
+//
+//  THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" 
+//  AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED 
+//  WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. 
+//  IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, 
+//  INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, 
+//  BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, 
+//  OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, 
+//  WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) 
+//  ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE 
+//  POSSIBILITY OF SUCH DAMAGE.
+//
+/////////////////////////////////////////////////////////////////////////////////////////
+
 */
 
 ///////////////////////////
